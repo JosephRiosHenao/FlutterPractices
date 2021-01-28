4ff15dd38d3ab6d6eb946ce95bb86cc5d79b7ca9
Joseph Rios Henao <joseph232005@gmail.com>
2021-01-28 15:02:18 -0500

    first commit

---
+++ flutter_practice_1/lib/back_gradient.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class back_gradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Stack(
+      children: <Widget>[
+        new gradient(),
+        new Positioned(
+            bottom: 0.0,
+            child: new Container(
+                width: MediaQuery.of(context).size.width,
+                height: 225.0,
+                color: Colors.white))
+      ],
+    );
+  }
+}
+
+class gradient extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return new Container(
+      decoration: new BoxDecoration(
+          gradient: new LinearGradient(
+              colors: [Color(0xFFffbb00), Color(0xFFf38c02)],
+              begin: const FractionalOffset(1.0, 0.1),
+              end: const FractionalOffset(1.0, 0.9))),
+    );
+  }
+}
---
+++ flutter_practice_1/lib/header.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class header extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(body: new Stack());
+  }
+}
