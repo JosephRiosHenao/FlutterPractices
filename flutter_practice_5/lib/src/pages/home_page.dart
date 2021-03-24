4f2b4ade579e0dc4e15d9c02853088d80990a48a
Joseph Rios Henao <joseph232005@gmail.com>
2021-03-23 21:56:29 -0500

    Manejando rutas

--- flutter_practice_5/lib/src/pages/home_page.dart
+++ flutter_practice_5/lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice_5/src/pages/alert_page.dart';
 import 'package:flutter_practice_5/src/providers/menu_provider.dart';
 import 'package:flutter_practice_5/src/utils/icon_string_util.dart';
 
@@ -22,22 +23,23 @@ class HomePage extends StatelessWidget {
       initialData: [],
       builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
         return ListView(
-          children: _listaItems(snapshot.data),
+          children: _listaItems(snapshot.data, context),
         );
       },
     );
   }
 
-  List<Widget> _listaItems(List<dynamic> data) {
+  List<Widget> _listaItems(List<dynamic> data, BuildContext context) {
     final List<Widget> opciones = [];
 
     data.forEach((opt) {
       final widgetTemp = ListTile(
-        title: Text(opt['texto']),
-        leading: getIcon(opt['icon']),
-        trailing: Icon(Icons.arrow_forward_ios_outlined, color: Colors.blue),
-        onTap: () {},
-      );
+          title: Text(opt['texto']),
+          leading: getIcon(opt['icon']),
+          trailing: Icon(Icons.arrow_forward_ios_outlined, color: Colors.blue),
+          onTap: () {
+            Navigator.pushNamed(context, opt['ruta']);
+          });
       opciones.add(widgetTemp);
     });
     return opciones;
